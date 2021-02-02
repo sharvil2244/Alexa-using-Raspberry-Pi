ac6b18da41b4abcfb276961fb8a4039abe301f8f
Sharvil Sopal <48448461+sharvil2244@users.noreply.github.com>
2021-02-02 11:33:50 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+apt-get update
+apt-get install libasound2-dev memcached mpg123 python-alsaaudio -y
+easy_install pip
+pip install -r requirements.txt
+cp initd_alexa.sh /etc/init.d/AlexaPi
+update-rc.d AlexaPi defaults
+touch /var/log/alexa.log
+
+echo "Enter your Device Type ID :"
+read productid
+echo ProductID = \"$productid\" >> creds.py
+
+echo "Enter your Security Profile Description:"
+read spd
+echo Security_Profile_Description = \"$spd\" >> creds.py
+
+echo "Enter your Security Profile ID:"
+read spid
+echo Security_Profile_ID = \"$spid\" >> creds.py
+
+echo "Enter your Security Client ID:"
+read cid
+echo Client_ID = \"$cid\" >> creds.py
+
+echo "Enter your Security Client Secret:"
+read secret
+echo Client_Secret = \"$secret\" >> creds.py
+
+ip=`hostname -I`
+python ./auth_web.py 
+echo "Open http://$ip:5000"
+
+echo "You can now reboot"
+
